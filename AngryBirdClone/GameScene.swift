401841fbcbb33e3e5ab09e43171cdfabc454e9a2
Onurcan Kurt <onurcankurt96@gmail.com>
2023-10-27 22:32:35 +0300

    Codes added for throwing the bird

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -21,6 +21,8 @@ class GameScene: SKScene {
     var box6 = SKSpriteNode()
     
     var gameStarted = false
+    
+    var originalPosition : CGPoint?
 
         
     
@@ -46,6 +48,7 @@ class GameScene: SKScene {
         bird.physicsBody?.affectedByGravity = false
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.15
+        originalPosition = bird.position
         
         //Box
         
@@ -160,6 +163,35 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if gameStarted == false{
+            
+            if let touch = touches.first {
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes{
+                        
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == bird {
+                                let dx = -(touchLocation.x - originalPosition!.x)
+                                let dy = -(touchLocation.y - originalPosition!.y)
+                                
+                                let impulse = CGVector(dx: dx, dy: dy)
+                                
+                                bird.physicsBody?.applyImpulse(impulse)
+                                bird.physicsBody?.affectedByGravity = true
+                                
+                                gameStarted = true
+                                
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
     }
     
